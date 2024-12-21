9350319df6543970c76b8d9b742dfc207a331e7b
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2024-12-21 12:28:07 +0530

    books json used

--- src/Mobile.js
+++ src/Mobile.js
@@ -1,22 +1,6 @@
 import React from 'react'
-import MobileList
- from './MobileList'
-
- const books = [{
-    id: 1,
-    title: "Harry Potter",
-    author: "J.K. Rowling",
-    price: 10.99,
-    image: "https://m.media-amazon.com/images/I/51UoqRAxwEL._AC_UF894,1000_QL80_.jpg"
-  },
-  {
-    id: 2,
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    price: 9.99,
-    image: "https://m.media-amazon.com/images/I/51UoqRAxwEL._AC_UF894,1000_QL80_.jpg"
- }]
-
+import MobileList from './MobileList'
+import books from './books.json'
 
 export default function Mobile() {
   return (
