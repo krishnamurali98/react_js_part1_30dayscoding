a937c8ac9e9c94daf257fc69cec5f9aa8fe62add
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2024-12-21 12:18:37 +0530

    const declaration for props

--- src/MobileList.js
+++ src/MobileList.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './MobileList.css'
 
 export default function MobileList(props) {
-
+    const {image, title, author, price} = props
     return (
     <div className='main'>
-      <img className='image' src={props.image} alt='Not found'/>
+      <img className='image' src={image} alt='Not found'/>
       <div>
-        <h2>{props.title}</h2>
-        <p>Author : {props.author}</p>
-        <p>Price : ${props.price}</p>
+        <h2>{title}</h2>
+        <p>Author : {author}</p>
+        <p>Price : ${price}</p>
         <button>Add to cart</button>
       </div>
     </div>
