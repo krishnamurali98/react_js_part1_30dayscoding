3e7fb95752f5cfaaef9647dd004c7008fb235397
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2024-12-21 11:43:56 +0530

    Intro to JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import './App.css';
 function App() {
+    let a = 2;
     return (
-        <h1>Hello I am from component</h1>
+        <div>
+            <h1 className='main'>let a = {a}</h1>
+            <h1>Hello I am from component</h1>
+            <p style={{backgroundColor: 'red', color: 'white'}}>This is React</p>
+        </div>
     );
 }
 
