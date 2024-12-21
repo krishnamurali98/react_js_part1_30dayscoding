ff9f5469d34e5ba2721ce6cf025dbb9c564257e1
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2024-12-21 12:16:13 +0530

    props with books

--- src/Mobile.js
+++ src/Mobile.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import MobileList
  from './MobileList'
+
+ const book1 = {
+    id: 1,
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    price: 10.99,
+    image: "https://m.media-amazon.com/images/I/51UoqRAxwEL._AC_UF894,1000_QL80_.jpg"
+  }
+  
+  const book2 = {
+    id: 2,
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    price: 9.99,
+    image: "https://m.media-amazon.com/images/I/51UoqRAxwEL._AC_UF894,1000_QL80_.jpg"
+ }
+
+
 export default function Mobile() {
   return (
     <div>
-        <MobileList />
-        <MobileList />
-        <MobileList />
+        <MobileList 
+            image ={book1.image} 
+            title={book1.title} 
+            author={book1.author} 
+            price={book1.price}
+        />
+        
+        <MobileList 
+            image ={book2.image} 
+            title={book2.title} 
+            author={book2.author} 
+            price={book2.price}
+        />
     </div>
   )
 }
--- src/MobileList.js
+++ src/MobileList.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import './MobileList.css'
 
-export default function MobileList() {
-    const image = 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/2/y/c/-original-imah4sssdf9pgz3e.jpeg?q=70';
-    const title = 'Samsung'
-    const price = 1211
+export default function MobileList(props) {
+
     return (
     <div className='main'>
-      <img className='image' src={image} alt='Not found'/>
+      <img className='image' src={props.image} alt='Not found'/>
       <div>
-        <h2>{title}</h2>
-        <p>Price : ${price}</p>
+        <h2>{props.title}</h2>
+        <p>Author : {props.author}</p>
+        <p>Price : ${props.price}</p>
         <button>Add to cart</button>
       </div>
     </div>
