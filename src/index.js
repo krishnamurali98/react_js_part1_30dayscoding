209c2a22c1cfd3d8464302e577c7a56cc13cb058
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2024-12-21 11:36:22 +0530

    Component changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    Hello World temporary
+    <App />
+    <App />
+    <App />
+    <App />
+    <App />
   </React.StrictMode>
 );
 
