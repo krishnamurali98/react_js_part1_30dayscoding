34158976ccc8b933e2ad1d1f17e2e0781d045e99
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2024-12-25 12:04:24 +0530

    Context API example1

--- src/A.js
+++ src/A.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import B from './B';
+
+const GreetContext = createContext();
 
 export default function A() {
     const greet = "Hello";
     return (
         <div>
-            <B greet={greet}/>
+            <GreetContext.Provider value={greet}>
+            </GreetContext.Provider>
         </div>
     )
 }
+
+export {GreetContext}
--- src/C.js
+++ src/C.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { GreetContext } from './A'
 
-export default function C(props) {
-    console.log(props);
-    
+export default function C() { 
     return (
-    <div>
-      {props.greet}
-    </div>
+        <GreetContext.Consumer>
+            {
+                (val) => {
+                    return <h1>Greet : {val}</h1>
+                }
+            }
+        </GreetContext.Consumer>
   )
 }
