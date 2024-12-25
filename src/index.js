0556ae246a5d19c37f9f929faf2f46e8824d23e5
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2024-12-25 11:06:43 +0530

    useEffect - Fetch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import UseEffect from './useEffect';
+import Fetch from './Fetch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UseEffect />
+    <Fetch />
   </React.StrictMode>
 );
 
