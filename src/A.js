5b18c08bb97aa1a3e3fa4ef2eb07abfa883815ca
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2024-12-25 12:10:14 +0530

    usecontext Example2

--- src/A.js
+++ src/A.js
@@ -2,15 +2,20 @@ import React, { createContext } from 'react'
 import B from './B';
 
 const GreetContext = createContext();
+const GreetContext2 = createContext();
 
 export default function A() {
     const greet = "Hello";
+    const greet2 = "World";
     return (
         <div>
             <GreetContext.Provider value={greet}>
+                <GreetContext2.Provider value={greet2}>
+                    <B />
+                </GreetContext2.Provider>
             </GreetContext.Provider>
         </div>
     )
 }
 
-export {GreetContext}
+export {GreetContext, GreetContext2}
--- src/C.js
+++ src/C.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { GreetContext } from './A'
+import { GreetContext, GreetContext2 } from './A'
 
 export default function C() { 
     return (
         <GreetContext.Consumer>
             {
                 (val) => {
-                    return <h1>Greet : {val}</h1>
+                    return (
+                        <GreetContext2.Consumer>
+                            {
+                                (val2) => {
+                                    return (
+                                        <div>
+                                            <h1>{val}</h1>
+                                            <h1>{val2}</h1>
+                                        </div>
+                                    );
+                                }
+                            }
+                        </GreetContext2.Consumer>
+                    );
                 }
             }
         </GreetContext.Consumer>
